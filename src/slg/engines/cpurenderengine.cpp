77622e4ddc624238b2ea5cc3b48046a2d943dab8
Sachin Keswani <sachin.keswani@amd.com>
2020-04-20 16:57:19 -0700

    Reverting back to processorCount=GetActiveProcessorCount() and calling processorCount variable as directly calling GetActiveProcessorCount() is causing build to fail.

--- src/slg/engines/cpurenderengine.cpp
+++ src/slg/engines/cpurenderengine.cpp
@@ -188,12 +188,18 @@ Properties CPURenderEngine::ToProperties(const Properties &cfg) {
 }
 
 const Properties &CPURenderEngine::GetDefaultProps() {
+//Check to see if processor group is present else default to old Windows 7 function.
+#if defined(_WIN32) || defined(WIN32) || defined(_WIN64) || defined(WIN64)
+	SYSTEM_INFO sysInfo;
+	GetSystemInfo(&sysInfo);
+	int processorCount = GetActiveProcessorCount(ALL_PROCESSOR_GROUPS);
+#endif
 	static Properties props = Properties() <<
-			RenderEngine::GetDefaultProps() <<
+		RenderEngine::GetDefaultProps() <<
 //For Windows version greater than Windows 7,modern way of calculating processor count is used 
 //May not work with Windows version prior to Windows 7
 #if defined(_WIN32) || defined(WIN32) || defined(_WIN64) || defined(WIN64)
-			Property("native.threads.count")(GetActiveProcessorCount(ALL_PROCESSOR_GROUPS));
+			Property("native.threads.count")(processorCount);
 #else 
 			Property("native.threads.count")(boost::thread::hardware_concurrency());
 #endif
